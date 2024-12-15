2983354cf78ab4c82c9f3af5b1348b2c2b8437e6
pieterjansen8 <pieterjansen8@icloud.com>
2024-12-15 13:25:38 +0100

    added dark / light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import type { Viewport } from 'next'
 import "./globals.css";
 
 const geistSans = Geist({
