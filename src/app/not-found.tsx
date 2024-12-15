e34b1e6dbd51f1abc29a938648abe645425f7cb1
pieterjansen8 <pieterjansen8@icloud.com>
2024-12-15 15:38:23 +0100

    added a custom 404 page!

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,25 +5,25 @@ import {useRouter} from 'next/navigation';
 import { Checkbox } from '@/components/ui/checkbox';
 export default function NotFound() {
   const router =  useRouter()
-  const setup = () => { 
+  const Setup = () => { 
     useEffect(() => {
       if(localStorage.getItem("auto-redirect")=="true"){
         router.push("/")
       }
     })
   }
-  function handlecheck(){
+  function Handlecheck(){
     useEffect(() => {
        localStorage.setItem("auto-redirect", "true")
        router.push("/")
     })
   }
-  setup()
+  Setup()
   return (
     <div className="grid  bg-slate-800 h-screen justify-items-center  sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <span className=' font-medium text-white text-lg sm:text-3xl mt-4'>ERROR  server can&apos;t find page: &quot;{window.location.pathname}&quot; ! </span>
       <div className="flex items-center space-x-2">
-        <Checkbox className='bg-white' id="terms" onCheckedChange={handlecheck}/>
+        <Checkbox className='bg-white' id="terms" onCheckedChange={Handlecheck}/>
         <label
           htmlFor="terms"
           className="text-sm font-medium text-white"
