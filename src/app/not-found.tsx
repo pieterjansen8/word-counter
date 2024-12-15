32720e4a19f98ccdd54246e96857097277f2a64c
pieterjansen8 <pieterjansen8@icloud.com>
2024-12-15 15:45:31 +0100

    added a custom 404 page!

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,20 +5,18 @@ import {useRouter} from 'next/navigation';
 import { Checkbox } from '@/components/ui/checkbox';
 export default function NotFound() {
   const router =  useRouter()
-  const Setup = () => { 
-    useEffect(() => {
-      if(localStorage.getItem("auto-redirect")=="true"){
-        router.push("/")
-      }
-    }, [])
-  }
+  useEffect(() => {
+    if(localStorage.getItem("auto-redirect")=="true"){
+      router.push("/")
+    }
+  }, [])
+  
   function Handlecheck(){
     useEffect(() => {
        localStorage.setItem("auto-redirect", "true")
        router.push("/")
     }, [])
   }
-  Setup()
   return (
     <div className="grid  bg-slate-800 h-screen justify-items-center  sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <span className=' font-medium text-white text-lg sm:text-3xl mt-4'>ERROR  server can&apos;t find page: &quot;{window.location.pathname}&quot; ! </span>
